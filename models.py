0f6be4cfb20cebbfda24ff41ca45ec66fe3a62ae
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-04-01 10:00:26 +0200

    I ceated the method in models .py

--- models.py
+++ models.py
@@ -32,21 +32,19 @@ class Project(models.Model):
     def __str__(self):
     	return self.name
 
-    def delete_image(self):
-    	self.delete()
 
-    def save_image(self):
+    def save_project(self):
     	self.save()
 
     @classmethod
-    def all_images(cls):
-        images = cls.objects.all()
-        return images 
+    def all_projets(cls):
+        projects = cls.objects.all()
+        return projects 
 
     @classmethod
-    def get_image(cls, id):
-        image = cls.objects.get(id=id)
-        return image
+    def get_project(cls, id):
+        project = cls.objects.get(id=id)
+        return project
     
     def __str__(self):
     	return self.user.username
@@ -54,8 +52,8 @@ class Project(models.Model):
     
     @classmethod
     def search_by_name(cls,search_term):
-        photos = cls.objects.filter(name__icontains=search_term)
-        return photos
+        projec = cls.objects.filter(name__icontains=search_term)
+        return projec
 
 class PhotosLetterRecipients(models.Model):
     name = models.CharField(max_length = 30)
@@ -67,15 +65,26 @@ class Rating(models.Model):
         design = models.CharField( null= True)
         usability   = models.CharField(max-length = 90,null=True)
         content = models.CharField( max-length = 90,null=True)
+        Project = models.ForeignKey(Project,null=True)
+        user = models.ForeignKey(User,null=True)
         
         def __str__(self):
-            return self.comment
+            return self.design
+         
+        def __str__(self):
+            return self.usability
 
-
-        def delete_comment(self):
-            self.delete()
-
-        def save_comment(self):
+        def __str__(self):
+            return self.content
+    
+        def save_rating(self):
             self.save()
 
+        def save_design(self):
+            self.save()
 
+        def save_usability(self):
+            self.save()
+        def save_content(self):
+            self.save()
+    
