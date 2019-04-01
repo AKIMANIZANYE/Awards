db9e426055871347ed812a166df5b97977e2eaba
akimanizanye.claudine@gmail.com <akimanizanye.claudine@gmail.com>
2019-04-01 10:20:03 +0200

    I added the foreign key

--- models.py
+++ models.py
@@ -9,8 +9,9 @@ from django.conf import settings
 class Profile(models.Model):
     bio = models.CharField(max_length =30)
     image = models.ImageField(upload_to='images/', blank=True)
-    Project = models.ForeignKey(Project,null=True)
     contact = models.EmailField(max_length=80,null=True)
+    Project_id = models.IntegerFlied(default=0)
+
 
     def __str__(self):
         return self.bio
@@ -22,11 +23,13 @@ class tags(models.Model):
         return self.name
 
 class Project(models.Model):
+    project_id = models.IntegerFlied(null=True)
     title = models.CharField(max_length = 30,null = True)
     landing = models.CharField(null = True)
     description = models.CharField(max_length = 100,null = True)
     link = models.CharField(max_length = 100,null = True)
     user = models.ForeignKey(User,null=True)
+    image = models.ForeignKey(Profile,null=True)
     Project = models.CharField(upload_to='images/', blank=True)
 
     def __str__(self):
@@ -65,8 +68,9 @@ class Rating(models.Model):
         design = models.CharField( null= True)
         usability   = models.CharField(max-length = 90,null=True)
         content = models.CharField( max-length = 90,null=True)
-        Project = models.ForeignKey(Project,null=True)
+        Project_id = models.ForeignKey(Project,null=True)
         user = models.ForeignKey(User,null=True)
+        image = models.ForeignKey(Profile,nul= True)
         
         def __str__(self):
             return self.design
